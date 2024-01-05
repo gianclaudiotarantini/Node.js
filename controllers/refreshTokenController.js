2123ecb43f93e56e7531986ab5482da86be90d68
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-01-05 20:05:34 +0100

    add controller logout

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -6,20 +6,20 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
-    const cookies = req.cookies;
+    const cookies = req.cookies;                                                // Estrazione dell'oggetto
     if (!cookies?.jwt) return res.sendStatus(401);
-    const refreshToken = cookies.jwt;
+    const refreshToken = cookies.jwt;                                   // Estrazione del valore del cookie
 
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
     if (!foundUser) return res.sendStatus(403); //Forbidden 
     // evaluate jwt 
-    jwt.verify(
+    jwt.verify(                                                     // Verifica della validità 
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);  // Se la verifica ha successo e il refreshToken è valido
             const accessToken = jwt.sign(
-                { "username": decoded.username },
+                { "username": decoded.username },                       // Allora viene genarato un nuovo token
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '30s' }
             );
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];  // Estrazione dell'intestazione
     if (!authHeader) return res.sendStatus(401);
     console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
